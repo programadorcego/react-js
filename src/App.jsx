b3239cba804ac31cc56deb9b303105c9fe7a258c
Programador Cego <programadorcego@gmail.com>
2024-11-02 08:44:08 -0300

    Aula 03: Componentes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <p>Olá universo!</p>
+            <p>Meu primeiro componente.</p>
+        </div>
+    );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-createRoot(document.querySelector("#root")).render(<p>Olá, universo!</p>);
+createRoot(document.querySelector("#root")).render(
+    <div>
+        <App />
+        <App />
+    </div>
+);
