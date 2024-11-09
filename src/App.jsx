1dc28bf250d7fe9f27b29a4436b9bc5e9c1e9ad4
Programador Cego <programadorcego@gmail.com>
2024-11-09 08:54:48 -0300

    Aula 05: Exibindo JavaScript

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
 const App = () => {
+    const user = {
+        name: "Willian",
+        lastname: "Pereira",
+        age: 34,
+    };
     return (
         <>
         <Header />
         <div>
-            <p>Conteúdo</p>
+            <p>Olá, meu nome é {user.name} {user.lastname} e tenho {user.age} anos.</p>
         </div>
         <Footer />
         </>
